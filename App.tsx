82fb66b0bc738a14e8c7df95f29a158016b2b3dc
Saket Roy <saket.roy@intellemo.com>
2025-01-15 00:43:57 +0530

    wip on screen animation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  useDrawerProgress,
+} from '@react-navigation/drawer';
 import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import {Button, Text, View} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import Animated, {interpolate, useAnimatedStyle} from 'react-native-reanimated';
+import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
 
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
@@ -12,7 +16,7 @@ const Stack = createStackNavigator();
 
 const HomeStackNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
     </Stack.Navigator>
@@ -21,7 +25,10 @@ const HomeStackNavigator = () => {
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
       <Tab.Screen name="Home" component={HomeStackNavigator} />
       <Tab.Screen name="Contact" component={ContactScreen} />
     </Tab.Navigator>
@@ -31,8 +38,32 @@ const BottomTabNavigator = () => {
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Drawer.Navigator drawerContent={DrawerContent}>
-        <Drawer.Screen name="Drawer" component={BottomTabNavigator} />
+      <Drawer.Navigator
+        drawerContent={DrawerContent}
+        screenOptions={({route}) => ({
+          headerShown: false,
+          drawerStyle: {
+            backgroundColor: '#1b1b2c',
+            width: 200,
+          },
+
+          drawerType: 'front',
+          swipeEdgeWidth: 30, // Edge swipe sensitivity
+          overlayColor: 'transparent', // Prevent default dark overlay
+        })}>
+        <Drawer.Screen
+          name="Drawer"
+          options={{
+            sceneStyle: {
+              backgroundColor: '#1b1b2c',
+            },
+          }}
+          component={() => (
+            <CustomAnimatedScreen>
+              <BottomTabNavigator />
+            </CustomAnimatedScreen>
+          )}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
@@ -45,6 +76,33 @@ const DrawerContent = () => {
     </View>
   );
 };
+
+function CustomAnimatedScreen({children}) {
+  const progress = useDrawerProgress();
+  const {top} = useSafeAreaInsets();
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const translateX = interpolate(progress.value, [0, 1], [0, 200 + 90]);
+    const translateY = interpolate(progress.value, [0, 1], [0, top]);
+    const rotateZ = interpolate(progress.value, [0, 1], [0, -10]);
+    const borderRadius = interpolate(progress.value, [0, 1], [0, 48]);
+    return {
+      transform: [{translateX}, {translateY}, {rotateZ: `${rotateZ}deg`}],
+      borderRadius,
+    };
+  });
+
+  return (
+    // <View>
+    <Animated.View
+      style={[
+        {flex: 1, backgroundColor: '#fafafa', overflow: 'hidden'},
+        animatedStyle,
+      ]}>
+      {children}
+    </Animated.View>
+  );
+}
 
 const HomeScreen = () => {
   const {navigate} = useNavigation();
